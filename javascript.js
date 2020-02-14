86b9887f45a3425d3b77f4bd8936187d313b4ef3
Michael Osgood <michael.f.osgood@gmail.com>
2020-02-14 14:23:16 -0800

    added comments explaining logic

--- javascript.js
+++ javascript.js
@@ -34,6 +34,8 @@ var quotes = [
  "Keep things fun."
 ]
 
+// Function is triggered when user clicks on button from index.html
+// Random quote from the array displays on the UI
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quotes.length));
     console.log("Random Number: " + randomNumber);
