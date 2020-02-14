b6a7a6b7be231fd06e70d581c4b82fe2da57e5bf
Michael Osgood <michael.f.osgood@gmail.com>
2020-02-14 13:45:15 -0800

    initial commit

---
+++ javascript.js
@@ -0,0 +1,44 @@
+// Created an array that contains all my 
+var quotes = [
+    "Do the right thing, ALWAYS.", 
+"Make quality personal.", 
+"Walk in your customer's shoes.", 
+"Work with a sense of urgency.",
+"Get clear on expectaions.",
+"Speak straight.",
+"Make the patient primary.",
+"Focus on others.",
+"Collaborate.",
+"Practice transparency.",
+"Listen generously.",
+"Treasure, promote and protect our reputation.",
+"Follow-up everything.",
+"Drive for innovation.",
+"Celebrate success.",
+"Think and act like an owner.",
+"Be a fanatic about response time.",
+"Deliver legendary service.",
+"Think team first.",
+"Honor commitments.",
+"Embrace change.",
+"Be relentless about improvement.",
+"Take ownership.",
+"Assume positive intent.",
+"Go the extra mile.",
+"Fix problems at the source.",
+"Take pride in appearance.",
+"Practice blameless problem solving.",
+"Always remember that we're a family",
+"Deliver results.",
+"'Bring It' every day.",
+ "Keep things fun."
+]
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    console.log("Random Number: " + randomNumber);
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
+
+
+
